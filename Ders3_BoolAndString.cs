f21ee7fb8b7ebb8ef40164b7ecf066f49b5a78e0
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-12-06 05:02:04 +0300

    Add files via upload

---
+++ Ders3_BoolAndString.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ders3_BoolAndString : MonoBehaviour
+{
+
+    string isim = "bedirhan";
+    bool karakterYerde;
+
+    private void Start()
+    {
+        Debug.Log(isim);
+        Debug.Log(karakterYerde);
+
+    }
+
+}
