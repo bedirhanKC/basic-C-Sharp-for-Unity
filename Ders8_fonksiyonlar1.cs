278149e6dddec141e726790a7c8f89c885c92bea
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-12-06 05:03:22 +0300

    Add files via upload

---
+++ Ders8_fonksiyonlar1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ders8_fonksiyonlar : MonoBehaviour
+{
+
+    public string yazi;
+
+    public int sayi1, sayi2;
+
+    public void toplamaFonksiyonu()
+    {
+
+        Debug.Log(sayi1 + sayi2);
+
+    }
+
+    public void konsolaYazdir()
+    {
+
+        Debug.Log(yazi);
+
+    }
+
+    
+}
