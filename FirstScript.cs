49ab7e04723087801c4122bedd55e1658a614c2c
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-12-06 05:01:26 +0300

    Add files via upload

---
+++ FirstScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+
+    private void Start() // oyun baþladýðýnda çalýþýr
+    {
+
+        Debug.Log("hello world! ");
+
+    }
+
+    private void Update() // belirli süre aralýklarýnda, sürekli çalýþýr
+    {
+
+        Debug.Log("printing...");
+        
+
+    }
+
+}
